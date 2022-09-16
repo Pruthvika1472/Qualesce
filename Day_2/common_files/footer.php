d1355427d2c2ed8809e947f9b40ed002e01e36d2
Pruthvika S <85168733+Pruthvika1472@users.noreply.github.com>
2022-09-16 12:07:58 +0530

    Add files via upload

---
+++ Day_2/common_files/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <a href="#" class="fa fa-facebook"></a>  
+    <a href="#" class="fa fa-twitter"></a>  
+    <a href="#" class="fa fa-linkedin"></a>  
+    <a href="#" class="fa fa-github"></a>
+    <a href="#" class="fa fa-instagram"></a>  
+    <a href="#" class="fa fa-youtube"></a>  
+     
+    
+        <p>© 2022 Anaconda Inc. All rights reserved.</p>
+        <br>
+        <h3>This website uses cookies to ensure you get the best experience on our website.<a href="#">Privacy Policy</a> </h3>
+</footer>
+</body>
+</html>
--- Day_2/common_files/header.php
+++ Day_2/common_files/header.php
@@ -1,1 +1,26 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
+</head>
+<body>
+<header>  
+    <h1>ANACONDA.</h1>
+    <nav>
+        <ul>
+            <li>Products</li>
+            <li>Pricing</li>
+            <li>Solutions</li>
+            <li>Resources</li>
+            <li>Partners</li>
+            <li>Blog</li>
+            <li>Company</li>
+        </ul>
+        <button>Contact Sales</button>
+    </nav>
+    </header>
