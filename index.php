f0244212b2a367095262c9f41b14c5548e6ffab1
Pruthvika S <85168733+Pruthvika1472@users.noreply.github.com>
2022-09-16 11:45:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    require_once('common_files/header.php');
+?>
+<h1>DAILY TASKS</h1>
+<ul>
+    <li><a href="Day_1/index.php" target="_top">Day_1 Task</a></li>
+    <li><a href="Day_2/index1.php" target="_blank">Day_2 Task</a></li>
+    <ol>
+         <li><a href="Day_2/index2.php" target="_blank">Task_1</a></li>
+         <li><a href="Day_2/index1.html" target="_blank">Task_2</a></li>
+         <li><a href="Day_2/index11.html" target="_blank">Task_3</a></li>
+    </ol>
+    <li><a href="Day_3/index.php" target="_blank">Day_3 Task</a></li>
+    <ol>
+
+         <li><a href="Day_3/table1.php" target="_blank">Task_1</a></li>
+         <li><a href="Day_3/form.php" target="_blank">Task_2</a></li>
+         <li><a href="Day_3/table2.php" target="_blank">Task_3</a></li>
+    </ol>
+   
+    <li><a href="Day_4/index.php" target="_blank">Day_4 Task</a></li>
+</ul>
+
+<?php
+    require_once('common_files/footer.php');
+?>
