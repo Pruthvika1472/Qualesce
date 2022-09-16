a1dd95e971f41d057ca2aee3284c1e341f2a3d03
Pruthvika S <85168733+Pruthvika1472@users.noreply.github.com>
2022-09-16 12:10:55 +0530

    Add files via upload

---
+++ Day_2/index1.php
@@ -0,0 +1,120 @@
+<?php
+ require_once('common files/header.php')
+?>
+<section>
+        <div class="heading"> <h2>Data science technology for<br>a better world. </h2></div> 
+        
+           <div>Anaconda offers the easiest way to perform Python/R data science and <br>machine learning on a single machine. Start working with thousands of <br>open-source packages and libraries today. </div>
+            <br>
+           
+</section>
+<button>Download</button>
+<p>For Windows<br>
+Python 3.9 • 64-Bit Graphical Installer • 594 MB</p>
+<section>
+        <h2> Not just<br>
+        point solutions.<br>
+        Powerful solving.</h2>
+        <p>Anaconda was built by data scientists, for data scientists. More than 20<br> million people use our technology to solve the toughest problems.</p>
+
+        <article>
+        <h3>Tackle any challenge</h3>
+        <p>Anaconda solutions are serious technology for real <br>
+            data science and ML applications. Anaconda is versatile <br>
+            -you'll be ready to solve problems you don't even know you have yet</p>
+        </article>
+
+        <article>
+        <h3>Own your destiny</h3>
+        <p>Your business challenges change every day. Only <br>open-source innovation can keep pace with your <br>needs. Never be stuck waiting for a vendor to add a <br>feature again.</p>
+        </article>
+
+        <article>
+        <h3>Stay safe and secure</h3>
+        <p>Catch vulnerabilities before they catch you. Control <br>access to models, data, and packages. Know the who,<br> what, when, and where of every project.</p>
+        <p>The modern world of data science is incredibly dynamic. Every<br> day, new challenges surface - and so do incredible<br> innovations. To win in this context, organizations need to give<br> their teams the most versatile, powerful data science and <br>machine learning technology so they can innovate fast - <br>without sacrificing security and governance.</p>
+        <br>
+        <p>We call that powerful solving.</p>
+        </article>
+
+</section>
+
+    <section>
+        <h4>REPORT</h4>
+        <h2>State of Data<br> Science: On the<br> Path to Impact</h2>
+        <p>Featuring insights from over 4,200 respondents, this year’s<br> State of Data Science report dives into data science industry<br> trends such as COVID-19’s impact, popular programming <br>languages, data literacy, and bias and explainability in machine<br> learning models.</p>
+        
+    </section>
+    <button>Get the Report</button>
+
+
+    <section>
+
+        <h2>Recent Articles</h2>
+        
+    </section>
+    <button>View More Articles</button>
+
+    <section>
+        <h2>Supercharge your data science <br>efforts with Anaconda.</h2>
+    </section>
+    <button>Get Started</button>
+
+    <section>
+        <h2>By data scientists, for <br>data scientists</h2>
+        <article>
+            <h4>PRODUCTS</h4>
+        <ul>
+            <li>Anaconda Distribution</li>
+            <li>Anaconda Professional</li>
+            <li>Anaconda Business</li>
+            <li>Anaconda Server</li>
+            <li>Enterprise DS Platform</li> 
+            <li>Professional Services</li>
+            <li>Pricing</li>
+        </ul>
+        </article>
+    
+        <article>
+            <h4>SOLUTIONS</h4>
+        <ul>
+            <li>Use Cases</li>
+            <li>Industries </li>
+           
+        </ul>
+        </article>
+ 
+        <article>
+            <h4>RESOURCES</h4>
+        <ul>
+            <li>Blog</li>
+            <li>Open Source</li>
+            <li>Library</li>
+            <li>Events</li>
+          
+        </ul>
+        </article>
+    
+        <article>
+            <h4>COMPANY</h4>
+        <ul>
+            <li>About Us</li>
+            <li>Leadership</li>
+            <li>Press</li>
+            <li>Contact Us</li>
+            <li>Careers</li>
+            <li>Anaconda Dividend</li>
+            <li>Partners</li>
+            <li>Anaconda FAQ</li>
+          
+        </ul>
+        </article>
+    </section>
+
+<?php
+ require_once('common files/footer.php')
+?>
+
+
+
+
---
+++ Day_2/index2.php
@@ -0,0 +1,168 @@
+<?php
+require_once('common files/header.php')
+?>
+<br>
+<section>
+    <h3>ANACONDA PRODUCTS</h3>
+    <h2>Pricing</h2>
+
+        <article>
+        <h2>Free</h2>
+            <p>Learn more<br>Students,academics,and hobbyists</p>          
+        </article>
+            <button>Get Started</button>
+
+        <article>
+            <h3>FREE</h3>
+            <hr>
+            <h3>Anaconda Distribution:</h3>
+            <ul>
+                <li>More than 8,000 DM/ML packages*</li>
+                <li>Conda pckg/env management system</li>
+                <li>Anaconda & Miniconda installers</li>
+                <li>Anaconda Navigator desktop GUI</li>
+                <li>OS: Windows, macOS, Linux</li>
+                <li> Architecture: x86, ARM, IBM/z, M1</li>
+                <li>Desktop integrations: Jupyter,<br> RStudio, VSCode, PyCharm, Spyder</li>
+            </ul>
+            <h3>Nucleus Community Membership: </h3>
+            <ul>
+                <li>Community content & support</li>
+                <li>Environment backup on Nucleus</li>
+            </ul>
+            <p>*Mirroring rights not included</p>
+        </article>
+            
+        
+        <article>
+            <h2>Pro</h2>
+            <p>Professionals</p>          
+        </article>
+            <button>Buy Now</button>
+        <article>
+            <h3>$14.95/mo**</h3>
+            <hr>
+            <h3>All the features of Free,plus:</h3>
+            <ul>
+                <li>Compliant for commercial use</li>
+                <li>Professional-grade repository</li>
+                <li>Conda signature verification</li>
+                <li>Tokenized user acess control</li>
+                <li>Basic package usage reporting</li>
+                
+            </ul>
+            <h3>Available add-ons: </h3><button>Contact us</button>
+            <ul>
+                <li>Site license</li>
+                <li>Custom private mirroring</li>
+                <li>Support services</li>
+                <li>Kickstart services</li>
+                <li>Long-term Support(LTS)</li>
+                <li>Jupyter notebook services</li>
+            </ul>
+            <p>**Get started now at our introductory rate,available for a limited time</p>
+            </article>
+    
+
+
+        <article>
+            <h2>Business</h2>
+            <p>Learn more<br>Professionals with security needs</p>          
+        </article>
+        <button>Buy Now</button>
+        <article>
+            <h3>$180/mo</h3>
+            <hr>
+            <h3>All the features of Pro,plus:</h3>
+            <ul>
+                <li>Curated vulnerability matching</li>
+                <li>Security policy & license filtering</li>
+                <li>Channel management</li>
+                <li>Role-based access control</li>
+                <li>Standard package usage reporting</li>
+                
+            </ul>
+            <h3>Available add-ons: </h3><button>Contact us</button>
+            <ul>
+                <li>Custom installers</li>
+                <li>Custom private mirroring</li>
+                <li>Support services</li>
+                <li>Kickstart services</li>
+                <li>Long-term Support(LTS)</li>
+                <li>Jupyter notebook services</li>
+            </ul>   
+        </article>
+
+
+        <article>
+            <h2>Enterprise</h2>
+            <p>Learn more<br>On-prem and private cloud users</p>          
+        </article>
+            
+        <button>Contact Us</button>
+        <article>
+            <h3>Custom</h3>
+            <hr>
+            <h3>All the features of Business,plus:</h3>
+            <ul>
+
+                    <li>On-prem repository & server</li>
+                    <li>Air-gap environment support</li>
+                    <li>Mirror CRAN, PyPI, & conda-forge</li>
+                    <li>Administer & track artifact history</li>
+                    <li>Support services</li>                
+                
+            </ul>
+            <h3>Available add-ons: </h3>
+            <ul>
+                <li>Site license</li>
+                <li>Custom installers</li>
+                <li>Implementation services</li>
+                <li>Premium support services</li>
+                <li>Kickstart services</li>
+                <li>Enterprise DS platform</li>
+                <li>Long-term Support (LTS)</li>
+            </ul>
+    
+            </article>
+</section>
+
+    <section>
+        <h2>More from Anaconda</h2>
+            <article>
+                <h3>Professional Services</h3>
+                    <p>Our professional services team is here for you. We will join <br>forces with your in-house experts to force-multiply your<br> efforts. Our consultants, developers, and engineers help you <br>solve heavy-weight data science and machine learning <br>challenges. Professional repo subscription required.</p>
+            </article>
+                <button>Learn More</button>
+            
+            <article>
+                <h3>Enterprise Data Science Platform</h3>
+                    <p>Anaconda Enterprise is a secure and scalable end-to-end<br> code development platform that empowers teams to create,<br> collaborate, and deploy projects in just one click. Build and<br> refine models as a team in a version and source-controlled<br> environment, with a wide selection of tools.</p>
+            </article>
+                <button>Learn More</button>
+
+            <article>
+            <h3>Anaconda Embedded</h3>
+            <p>Our Embedded Partner Program allows you the choice to<br> embed Anaconda within your backend systems, enable your<br> customers to create managed Python environments, or<br> provide customers with access to the full Anaconda<br> repository.</p>
+            </article>
+            <button>Learn More</button>
+
+
+            <article>
+            <h3>Government Repository</h3>
+            <p>Anaconda enables government entities and institutions to <br>leverage open-source innovation for data science with a<br> custom-curated, privately-hosted repository of open-source<br> packages and artifacts. Contact us to learn more or request a <br>proposal.</p>
+            </article>
+            <button>Contact Us</button>
+            <br>
+            <br>
+    </section>
+<?php
+require_once('common files/footer.php')
+?>
+
+
+
+
+
+
+
